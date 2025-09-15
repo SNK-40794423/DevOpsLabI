37131af5365b41c3d3d8388a54be5103af4cc97f
SNK-40794423 <40794423@live.napier.ac.uk>
2025-09-15 16:18:06 +0630

    Ixacise commit testing
    Signed-off-by: SNK-40794423 <40794423@live.napier.ac.uk>

---
+++ src/main/java/imc/com/Ixacise.java
@@ -0,0 +1,9 @@
+package imc.com;
+
+public class Ixacise {
+    public static void main(String[] args) {
+        System.out.println("Fight for Kana, Kana no tame ni nara\n" +
+                "Subete wo giseni shitemo ii\n" +
+                "Who can control, White Justice Individual System");
+    }
+}
