c5729c160a84240a3a001beebd876a5e0b398d37
SNK-40794423 <40794423@live.napier.ac.uk>
2025-09-29 15:41:17 +0630

    view-record app update
    Signed-off-by: SNK-40794423 <40794423@live.napier.ac.uk>

--- src/main/java/com/testproject/App.java
+++ src/main/java/com/testproject/App.java
@@ -117,4 +117,19 @@ public class App
                             + "Manager: " + emp.manager + "\n");
         }
     }
+    public static void main(String[] args)
+    {
+        // Create new Application
+        App a = new App();
+
+
+        // Connect to database
+        a.connect();
+        // Get Employee
+        Employee emp = a.getEmployee(255530);
+        // Display results
+        a.displayEmployee(emp);
+        // Disconnect from database
+        a.disconnect();
+    }
 }
