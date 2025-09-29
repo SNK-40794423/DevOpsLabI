6e0e37d1286c03ee64e059610ff8f70057abf414
SNK-40794423 <40794423@live.napier.ac.uk>
2025-09-29 15:22:14 +0630

    view-record app update
    Signed-off-by: SNK-40794423 <40794423@live.napier.ac.uk>

--- src/main/java/com/testproject/Main.java
+++ src/main/java/com/testproject/Main.java
@@ -3,9 +3,18 @@ package com.testproject;
 //TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
 // click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 public class Main {
-    public static void main(String[] args) {
-        //TIP Press <shortcut actionId="ShowIntentionActions"/> with your caret at the highlighted text
-        // to see how IntelliJ IDEA suggests fixing it.
-        System.out.print("Boo Ya");
+    public static void main(String[] args)
+    {
+        // Create new Application
+        App a = new App();
+
+        // Connect to database
+        a.connect();
+        // Get Employee
+        Employee emp = a.getEmployee(255530);
+        // Display results
+        a.displayEmployee(emp);
+        // Disconnect from database
+        a.disconnect();
     }
 }
