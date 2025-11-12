0cd7c83b7f1aa0126c9de7797bb543104f34dd31
SNK-40794423 <40794423@live.napier.ac.uk>
2025-11-12 16:12:25 +0630

    Lab7 testAddEmployee push
    Signed-off-by: SNK-40794423 <40794423@live.napier.ac.uk>

--- src/test/java/com/testproject/AppIntegrationTest.java
+++ src/test/java/com/testproject/AppIntegrationTest.java
@@ -33,4 +33,18 @@ public class AppIntegrationTest {
         assertEquals(department.dept_no, "d002");
         assertEquals(department.dept_name, "Finance");
     }
+
+    @Test
+    void testAddEmployee()
+    {
+        Employee emp = new Employee();
+        emp.emp_no = 500000;
+        emp.first_name = "Kevin";
+        emp.last_name = "Chalmers";
+        app.addEmployee(emp);
+        emp = app.getEmployee(500000);
+        assertEquals(emp.emp_no, 500000);
+        assertEquals(emp.first_name, "Kevin");
+        assertEquals(emp.last_name, "Chalmers");
+    }
 }
